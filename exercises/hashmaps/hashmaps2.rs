51c41e12a51082bccaac9f735a225079006cec3a
Mappland <mapplandli@gmail.com>
2024-04-30 18:37:35 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -34,12 +34,17 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::Peach,
+        Fruit::Grape,
+        Fruit::Watermalon,
+        Fruit::Cherry
     ];
 
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if 
     }
 }
 
